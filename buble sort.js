6f935c382d9dd6886e46be90100979338922dedc
aymanezahir2 <aymanezahir2@gmail.com>
2022-03-28 01:59:03 +0000

    comment

--- buble sort.js
+++ buble sort.js
@@ -1,13 +1,18 @@
 function BubleSort(acc){
+    // this for help loop j to stop a point which variable
     for(let i =0; i<acc.length ;i++)
     {
+        // this is response of change value
         for(let j =0;j<acc.length - i -1;j++)
         {
+            // this simple condition
             if(acc[j+1]<acc[j]){
+                // this is sawping if don't know go to elzero channel youtube a swaping Array
                 [acc[j+1],acc[j]]=[acc[j],acc[j+1]]
             }
         };
     };
+    // return params value
     return acc
 };
 
